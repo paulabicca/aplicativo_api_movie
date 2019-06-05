dc39d809e2cc09fcace670ebf91aa51890112bdb
Paula Bicca <paula.bicca@gmail.com>
2019-06-05 13:54:59 -0300

    App completo

---
+++ src/app/pages/movie-details/movie-details.page.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MovieService } from './../../services/movie.service';
+@Component({
+  selector: 'app-movie-details',
+  templateUrl: './movie-details.page.html',
+  styleUrls: ['./movie-details.page.scss'],
+})
+export class MovieDetailsPage implements OnInit {
+  information = null;
+
+  constructor(private activatedRoute: ActivatedRoute, private movieService: MovieService) { }
+
+  ngOnInit() {
+    let id = this.activatedRoute.snapshot.paramMap.get('id');
+
+    this.movieService.getDetails(id).subscribe(result => {
+      console.log('details:', result);
+      this.information = result;
+    });
+  }
+  openWebSite(){
+    window.open(this.information.Website, '_blank');
+  }
+}
